e4fcf1173654bfb50f818f603c164996f72848fb
Gudata <i.bardarov@gmail.com>
2012-06-14 16:54:32 +0300

    Added Bulgarian

---
+++ localization/jquery-ui-timepicker-bg.js
@@ -0,0 +1,20 @@
+/* Russian translation for the jQuery Timepicker Addon */
+/* Written by Trent Richardson */
+(function($) {
+	$.timepicker.regional['ru'] = {
+		timeOnlyTitle: 'Изберете време',
+		timeText: 'Време',
+		hourText: 'Час',
+		minuteText: 'Минути',
+		secondText: 'Секунди',
+		millisecText: 'Милисекунди',
+		timezoneText: 'Часови пояс',
+		currentText: 'Сега',
+		closeText: 'Затвори',
+		timeFormat: 'hh:mm tt',
+		amNames: ['AM', 'A'],
+		pmNames: ['PM', 'P'],
+		ampm: false
+	};
+	$.timepicker.setDefaults($.timepicker.regional['bg']);
+})(jQuery);
